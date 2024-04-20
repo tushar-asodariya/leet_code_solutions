105108ef563455e722714800e1c07574edb33f0f
Tushar Asodariya <tusharasodariya1@gmail.com>
2024-04-20 11:39:49 +0530

    Time: 349 ms (86.29%) | Memory: 152.4 MB (8.20%) - LeetSync

---
+++ 1-two-sum/two-sum.dart
@@ -0,0 +1,17 @@
+import "dart:collection";
+class Solution {
+  List<int> twoSum(List<int> nums, int target) {
+    Map<int, int> prevMap = HashMap();
+
+    for(int i=0;i<nums.length;i++){
+        int value = target - nums[i];
+        if(prevMap.containsKey(value)){
+            return [i,prevMap[value]!];
+        }
+        else{
+            prevMap[nums[i]] = i;
+        }
+    }
+    return [0,0];
+  }
+}
