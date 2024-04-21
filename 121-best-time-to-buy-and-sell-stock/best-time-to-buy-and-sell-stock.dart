9d344598f4d89c1e27910ae095a11f933c32cbca
Tushar Asodariya <tusharasodariya1@gmail.com>
2024-04-21 10:33:36 +0530

    Time: 422 ms (25.10%) | Memory: 188.2 MB (95.33%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.dart
@@ -0,0 +1,24 @@
+class Solution {
+  int maxProfit(List<int> prices) {
+    int maxProfit = 0;
+    if(prices.length<=1){
+        return maxProfit;
+    }
+    int l = 0, r = 0;
+
+   while(r < prices.length){
+    if(prices[r] < prices[l]){
+        l++;
+        r=l+1;
+    }else{
+        int currentProfit = prices[r]-prices[l];
+        if(currentProfit>maxProfit){
+            maxProfit = currentProfit;
+        }
+            r++;
+
+    }
+   }
+    return maxProfit;
+  }
+}
