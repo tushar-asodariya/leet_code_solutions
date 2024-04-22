d3d50ac6c67b9fa5ad3b18e6c8b8a10a541b51c1
Tushar Asodariya <tusharasodariya1@gmail.com>
2024-04-22 09:50:21 +0530

    Time: 382 ms (74.22%) | Memory: 178 MB (68.63%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.dart
+++ 217-contains-duplicate/contains-duplicate.dart
@@ -1,6 +1,18 @@
 class Solution {
   bool containsDuplicate(List<int> nums) {
-    Set<int> numSet = nums.toSet();
-    return nums.length != numSet.length;
+    Set<int> numSet = {};
+    bool isDuplicate = false;
+
+    for(var x in nums){
+ if(numSet.contains(x)){
+            isDuplicate= true;
+            break;
+        }
+        numSet.add(x);
+    }
+   
+
+    return isDuplicate;
+
   }
 }
