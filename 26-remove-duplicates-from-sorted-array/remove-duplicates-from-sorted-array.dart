0d07d5f92e0cb06a2174c8cdf5f5789f8c377bb0
Tushar Asodariya <tusharasodariya1@gmail.com>
2024-05-31 08:49:41 +0530

    Time: 378 ms (52.21%) | Memory: 151.1 MB (39.56%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.dart
@@ -0,0 +1,16 @@
+class Solution {
+  int removeDuplicates(List<int> nums) {
+    int i = 1;
+    if(nums.isEmpty){
+        return 0;
+    }
+
+    for(var j=0; j<nums.length;j++){
+        if(j+1!=nums.length && nums[j]!=nums[j+1]){
+            nums[i] = nums[j+1];
+            i++;
+        }
+    }
+    return i;
+  }
+}
