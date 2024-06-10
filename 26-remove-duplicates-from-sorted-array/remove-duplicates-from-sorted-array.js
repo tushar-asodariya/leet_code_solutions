2453add83d77a2fa035a4464d05c8ee855feeb60
Tushar Asodariya <tusharasodariya1@gmail.com>
2024-06-10 23:21:58 +0530

    Time: 66 ms (51.47%) | Memory: 51.6 MB (88.64%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+    var j = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] != nums[i - 1]) {
+            nums[j] = nums[i]
+            j++
+        }
+    }
+    return j;
+};
