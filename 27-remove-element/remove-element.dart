a118cfec42a5312f035f76d3a7bfc61d6ffe03a1
Tushar Asodariya <tusharasodariya1@gmail.com>
2024-06-10 23:42:28 +0530

    Time: 332 ms (63.23%) | Memory: 146.6 MB (95.52%) - LeetSync

--- 27-remove-element/remove-element.dart
+++ 27-remove-element/remove-element.dart
@@ -3,8 +3,8 @@ class Solution {
    int i = 0;
     for(int j=0;j<nums.length;j++){
         if(nums[j]!=val ){
-            nums[i]=nums[j];
-            i++;
+            nums[i++]=nums[j];
+            
         }
     }
     return i;
