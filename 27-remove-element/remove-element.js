f71e724fa84b0abb8d5301d372975b4476ea2531
Tushar Asodariya <tusharasodariya1@gmail.com>
2025-07-29 22:17:53 +0530

    Time: 0 ms (100.00%) | Memory: 53.8 MB (33.35%) - LeetSync

--- 27-remove-element/remove-element.js
+++ 27-remove-element/remove-element.js
@@ -7,16 +7,20 @@ var removeElement = function(nums, val) {
     let j = 0;
     for(let i=0; i< nums.length;i++){
 
-        if(nums[i] == val  ){
-            if(nums[i+1]!=undefined && nums[i+1]!=val){
-           nums[j] = nums[i+1]      
-            j++
-            i++
-            }
-        }else{
+        // if(nums[i] == val  ){
+        //     if(nums[i+1]!=undefined && nums[i+1]!=val){
+        //    nums[j] = nums[i+1]      
+        //     j++
+        //     i++
+        //     }
+        // }else{
 
-            nums[j]= nums[i]
-            j++
+        //     nums[j]= nums[i]
+        //     j++
+        // }
+        if(nums[i] !== val){
+            nums[j++] = nums[i]
+
         }
     }
     return j
